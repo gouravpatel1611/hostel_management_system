6d3b3bcebd0e08e54257ede2c2771d302dc7b8a0
gourav <gourav_patel>
2022-07-21 23:53:30 +0530

    disebale csrf

--- Hostel_management_system/settings.py
+++ Hostel_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z=5sgpy83fq5nwro*)&=1*!+%f7$!&d)d+y!^&2db=pv-#yaty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8a445cd1ea2fcf.lhrtunnel.link','*']
 
 
 # Application definition
@@ -44,10 +44,10 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'Hostel_management_system.urls'
